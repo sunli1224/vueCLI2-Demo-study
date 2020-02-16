bb34fca71d46fa9705825d118ae48c27ca22748a
sunli1224 <youdianxiaoshuai24@126.com>
2020-02-17 04:10:58 +0800

    vueCLI2 tobar组件封装 sunli

--- vue-cli2-demo03/src/App.vue
+++ vue-cli2-demo03/src/App.vue
@@ -1,13 +1,42 @@
 <template>
   <div id="app">
+    <TabBar>
+      <TabBarItem>
+        <div slot="item-icon-active">img1-active</div>
+        <div slot="item-icon">img1</div>
+        <div slot="item-text">首页</div>
+      </TabBarItem>
+      <TabBarItem>
+        <div slot="item-icon-active">img2-active</div>
+        <div slot="item-icon">img2</div>
+        <div slot="item-text">分类</div>
+      </TabBarItem>
+      <TabBarItem>
+        <div slot="item-icon-active">img3-active</div>
+        <div slot="item-icon">img3</div>
+        <div slot="item-text">购物车</div>
+      </TabBarItem>
+      <TabBarItem>
+        <div slot="item-icon-active">img4-active</div>
+        <div slot="item-icon">img4</div>
+        <div slot="item-text">我的</div>
+      </TabBarItem>
+    </TabBar>
   </div>
 </template>
 
 <script>
+import TabBar from './components/tabar/TabBar'
+import TabBarItem from './components/tabar/TabBarItem/TabBarItem'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TabBar,
+    TabBarItem
+  }
 }
 </script>
 
 <style>
+  @import "./assets/css/base.css";
 </style>
---
+++ vue-cli2-demo03/src/components/tabar/TabBarItem/TabBarItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div @click="judgeActive()" class="tar-bar-item">
+      <div  v-if="!isActive">
+        <slot name="item-icon-active"></slot>
+      </div>
+      <div v-else>
+        <slot name="item-icon"></slot>
+      </div>
+      <div :class="{active: !isActive}">
+        <slot name="item-text"></slot>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  data () {
+    return {
+      isActive: true
+    }
+  },
+  methods: {
+    judgeActive () {
+      this.isActive = !this.isActive
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tar-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+  .active {
+    color: pink;
+  }
+</style>
